73a55e54857a0b2d7dcfe305ae88e3d39101b265
NorbertCorpuz <145172710+NorbertCorpuz@users.noreply.github.com>
2023-12-14 05:16:42 +0800

    Update motor-run.py

--- motor-run.py
+++ motor-run.py
@@ -3,6 +3,7 @@ import firebase_admin
 from firebase_admin import db
 from firebase_admin import credentials
 
+# Setuo Database
 cred = credentials.Certificate('/home/norbie/Desktop/emtech/credentials.json')
 
 options = {
@@ -11,25 +12,25 @@ options = {
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred,options)
 
+
+# Define motor
 motor = motor(Ena=25, In1=24, In2=23)
-
-
-
 while True:
-    run_ref = db.reference("/motor_run")
-    run_data = run_ref.get()
-
-
-    if run_data == True:
-
-
-        motor.move_forward(speed=100, t=.3)
+    
+    # Get value of run (True of False)
+    run = db.reference("/motor_run")
+    
+    if run == True:
+        motor.move_forward(speed=100, t=.8)
         motor.stop(.5)
-        motor.stop(5)
-        motor.move_backward(speed=100, t=.3)
+        motor.move_forward(speed=100, t=.2)
+        motor.move_backward(speed=100, t=1.05)
         motor.stop()
+        # Update database to stop motor
+        db.reference("/").update({"motor_run": False}) 
 
-
+    
+        
     else:
         continue
 
